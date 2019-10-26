0ebbde8270bd38360e09eb661b78a6a235c84d78
Kiran <kodeutility@gmail.com>
2019-10-26 23:34:28 +0530

    used Card component with props

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Card } from "../card/card.component";
 import "./card-list.styles.css";
 
 export const CardList = props => {
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
-        <h1 key={monster.email}>{monster.name}</h1>
+        <Card email={monster.email} name={monster.name} />
       ))}
     </div>
   );
