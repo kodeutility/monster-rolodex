5b82e1e5c03cd87dc688c90e8cb79d27b8931458
Kiran <kodeutility@gmail.com>
2019-10-26 23:58:01 +0530

    used email in paragraph tag

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import "./card.styles.css";
 export const Card = props => (
   <div className="card-container">
     <img alt="monster" src={`https://robohash.org/${props.id}?set=set2`} />
-    <h1 key={props.id}>{props.name}</h1>
+    <h2 key={props.id}>{props.name}</h2>
+    <p>{props.email}</p>
   </div>
 );
