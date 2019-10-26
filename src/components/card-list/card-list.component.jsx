d501903abe76e658b22294efdcddd26d5f7396a8
Kiran <kodeutility@gmail.com>
2019-10-26 23:57:41 +0530

    sent email as prop

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ export const CardList = props => {
   return (
     <div className="card-list">
       {props.monsters.map(monster => (
-        <Card id={monster.id} name={monster.name} />
+        <Card id={monster.id} name={monster.name} email={monster.email} />
       ))}
     </div>
   );
