9f380d7c3581f2d4b5adc86aa19fb5f55ef55907
Kiran <kodeutility@gmail.com>
2019-10-26 23:24:17 +0530

    use props to generate individual card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,11 @@ import React, { Component } from "react";
 import "./card-list.styles.css";
 
 export const CardList = props => {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <h1 key={monster.email}>{monster.name}</h1>
+      ))}
+    </div>
+  );
 };
