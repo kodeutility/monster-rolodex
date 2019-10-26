873fc1817a8c0a5ec0c7ead7e2b34e2d43161451
Kiran <kodeutility@gmail.com>
2019-10-26 23:54:33 +0530

    removed console.log

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,6 @@ import "./card.styles.css";
 
 export const Card = props => (
   <div className="card-container">
-    {console.log(props)}
     <img alt="monster" src={`https://robohash.org/${props.id}?set=set2`} />
     <h1 key={props.id}>{props.name}</h1>
   </div>
