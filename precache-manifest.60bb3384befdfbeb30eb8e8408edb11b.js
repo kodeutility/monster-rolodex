5aa7d49396adb9c2554cdb195ec37fdf79bb7a88
Kiran <kodeutility@gmail.com>
2019-11-02 00:12:41 +0530

    Updates

---
+++ precache-manifest.60bb3384befdfbeb30eb8e8408edb11b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179ec6b3dcac3f1fb15f090ece56e9cf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "afce7aa719233922f3da",
+    "url": "/monster-rolodex/static/css/main.580497cf.chunk.css"
+  },
+  {
+    "revision": "b0b58fe607faa4d03a43",
+    "url": "/monster-rolodex/static/js/2.de1aa811.chunk.js"
+  },
+  {
+    "revision": "afce7aa719233922f3da",
+    "url": "/monster-rolodex/static/js/main.8d98f614.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
